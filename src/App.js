9458b2cf0340b2ecb00147fb8a98b08ea5230bd4
Leonardo Diegues <dscleo@gmail.com>
2020-06-19 22:08:02 -0300

    initial commi8t

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    }, []);
+  });
+
+  async function handleAddRepository(title, url, techs) {
+    const response = await api.post('repositories', {
+      title: `New repository: ${Date.now()}`,
+      url: `http://github.com/leonardodiegues`,
+      techs: ['NodeJS', 'ReactJS'],
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const endpoint = `repositories/${id}`;
+    const _ = await api.delete(endpoint);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
+        {repositories.map(repo => <li key={repo.id}>{repo.title}</li>)}
         <li>
-          Repositório 1
-
           <button onClick={() => handleRemoveRepository(1)}>
             Remover
           </button>
